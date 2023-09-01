53863b4bfda5515f8da8f2e595373f6bd24c2cfd
bohun <slawkd2002@gmail.com>
2023-09-01 18:19:42 +0900

    [Update]
    1.플레이어 이동
    2.타일맵 만들기

---
+++ GoldMetal/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+
+    public float Speed;
+
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigid;
+    Animator anim;
+
+    private void Awake()
+    {
+        rigid  = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // 위치 이동
+        Vector2 nextVec = inputVec.normalized * Time.deltaTime*Speed;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+        if (inputVec.x!=0)
+        {
+            spriteRenderer.flipX = inputVec.x<0;
+        }
+    }
+}
