b908c64a4992981e472d029a0add0792a2f32c17
bohun <slawkd2002@gmail.com>
2023-09-03 14:13:31 +0900

    [Update]
    1.무한맵
    2.카메라 무빙

--- GoldMetal/Assets/Scripts/GameManager.cs
+++ GoldMetal/Assets/Scripts/GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
     public Player player;
 
+    private void Awake()
+    {
+        Instance = this;
+    }
 }
