244cca677a5d07e5d917f79eaec8260c33b91f73
bohun <slawkd2002@gmail.com>
2023-10-07 15:28:16 +0900

    플레이어공격
    [Update]
    1. 적 처치시 경험치 증가

--- GoldMetal/Assets/Scripts/Enemy.cs
+++ GoldMetal/Assets/Scripts/Enemy.cs
@@ -69,7 +69,7 @@ public class Enemy : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Bullet"))
+        if (!collision.CompareTag("Bullet") || !isLive)
             return;
 
         health -= collision.GetComponent<Bullet>().damage;
@@ -86,6 +86,8 @@ public class Enemy : MonoBehaviour
             rigid.simulated = false;
             spriter.sortingOrder = 1;
             anim.SetBool("Dead", true);
+            GameManager.Instance.kill++;
+            GameManager.Instance.GetExp();
         }
     }
 
--- GoldMetal/Assets/Scripts/GameManager.cs
+++ GoldMetal/Assets/Scripts/GameManager.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-
+    [Header("# Game Control")]
     public float gameTime;
     public float maxGameTime = 2 * 10f;
-
+    [Header("#  Player Info")]
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = { 3,5,10, 30, 60, 150, 280, 350, 450, 600 };
+    [Header("# Player Object")]
     public PoolManager pool;
     public Player player;
 
@@ -23,6 +28,17 @@ public class GameManager : MonoBehaviour
         if (gameTime > maxGameTime)
         {
             gameTime = maxGameTime;
+        }
+    }
+
+    public void GetExp()
+    {
+        exp++;
+        if(exp == nextExp[level])
+        {
+            level++;
+            exp = 0;
+
         }
     }
 }
